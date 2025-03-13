36bb6b8fad8936217b686ee368290eaef9e56e0b
doppio-movimento <doppiomovimento@proton.me>
2025-03-13 13:25:47 -0400

    finish the interface design plz?

--- fe/src/routes/home.jsx
+++ fe/src/routes/home.jsx
@@ -2,11 +2,20 @@ const Home = (props) => {
     return (
         <div className="flex w-full h-full place-items-center gap-2">
             <div className="flex flex-col h-full w-1/9 gap-2 p-2">
-                <div className="h-1/3 border border-teal-900 w-full"></div>
+                <div className="h-1/3 border border-teal-900 w-full">
+                    <div className="flex flex-col h-full">
+                        <div className="flex justify-center place-items-center text-sm font-mono text-white pt-3 h-1/5">
+                            Mode
+                        </div>
+                        <div className="flex justify-center place-items-center h-4/5">
+                            <div className="h-7/9 bg-black w-5 rounded-2xl"></div>
+                        </div> 
+                    </div>
+                </div>
                 <div className="h-1/3 border border-teal-900 w-full"></div>
             </div>
             <div className="h-full w-6/9 overflow-hidden flex justify-center place-items-center rounded">
-                <img src={"/blackhole.png"} alt="asciify it"/>
+                <img src={'/blackhole.png'} alt="asciify it" />
             </div>
             <div className="h-full w-2/9 flex flex-col gap-2 p-2">
                 <div className="h-2/5 border border-teal-900"></div>
