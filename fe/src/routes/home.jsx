edf740cc7bf67ec99255cdf4dcd3a6495ef9ebba
doppio-movimento <doppiomovimento@proton.me>
2025-03-13 16:34:14 -0400

    grayscale

--- fe/src/routes/home.jsx
+++ fe/src/routes/home.jsx
@@ -82,10 +82,34 @@ const Home = (props) => {
                     </div>
                 </div>
                 <div className="h-1/5 border border-teal-900">
-                    <div className="flex flex-col h-full place-items-center gap-5">
-                        <div className="flex pt-3 font-mono text-sm text-white h-1/7">
+                    <div className="flex flex-col h-full place-items-center gap-1">
+                        <div className="flex pt-2 font-mono text-sm text-white h-1/4">
                             Grayscale
                         </div>
+                        <div className="h-2/4 w-full border border-white flex flex-col gap-3">
+                            <div className="flex justify-between px-5">
+                                <div className="font-mono text-xs text-green-400">
+                                    Standard
+                                </div>
+                                <div className="font-mono text-xs text-green-700">
+                                    Custom
+                                </div>
+                            </div>
+                            <div className="flex justify-between px-5">
+                                <div className="flex gap-2">
+                                    <div className="w-6 h-6 bg-gray-400 border border-green-400 flex justify-center place-items-center text-sm font-mono">
+                                        70
+                                    </div>
+                                    <div className="font-mono text-xs text-yellow-500 flex justify-center place-items-center">
+                                        chars
+                                    </div>
+                                </div>
+                                <div className="text-yellow-700 font-mono text-xs flex justify-center place-items-center">
+                                    Input chars
+                                </div>
+                            </div>
+                        </div>
+                        <div className="h-1/4 w-full border border-white"></div>
                     </div>
                 </div>
                 <div className="h-2/5 border border-teal-900"></div>
