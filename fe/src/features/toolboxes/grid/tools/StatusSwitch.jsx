cb6c88cf50292b9852ea3ac942d2f4bc61db6692
doppio-movimento <doppiomovimento@proton.me>
2025-03-15 13:10:10 -0400

    basic bitch switch

--- fe/src/features/toolboxes/grid/tools/StatusSwitch.jsx
+++ fe/src/features/toolboxes/grid/tools/StatusSwitch.jsx
@@ -6,14 +6,12 @@ const StatusSwitch = (props) => {
     return (
         <div className="relative h-1/7 w-full flex justify-center">
             <div
-                className="w-4/7 bg-black h-4 rounded-xl bg-cyan-950 relative"
+                className="w-4/7 bg-black h-4 rounded-xl relative"
+                style={{backgroundColor: gridOn ? "blue" : "orange"}}
                 onClick={() => setGridOn(!gridOn)}
             >
-                <div className="absolute top-[50%] translate-y-[-50%] z-10 w-7 h-7 rounded-full bg-cyan-400 text-xs flex justify-center place-items-center">
-                    ON
-                </div>
-                <div className="absolute top-[50%] translate-y-[-50%] right-4 text-amber-500 font-mono text-xs">
-                    Turn off
+                <div className="absolute top-[50%] translate-y-[-50%] z-10 w-7 h-7 rounded-full text-xs flex justify-center place-items-center" style={{ left: gridOn ? 0 : "85%", backgroundColor: gridOn ? "cyan" : "red"}}>
+        {gridOn ? "ON" : "OFF"}
                 </div>
             </div>
         </div>
